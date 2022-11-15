69e01dcf0480f6fb60a862f3907c400077b7d5c6
“nidumolu” <snidumolu@gmail.com>
2022-11-15 17:07:01 +0530

    initial commit

---
+++ FirstProgram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+//using System.Windows.Forms;
+//namespace SeeSharp
+
+class FirstProgram
+{
+    static void Main()
+    {
+        //MessageBox.Show("Hello, World!");
+        Console.WriteLine("Is almost the same argument!");
+    }
+}
